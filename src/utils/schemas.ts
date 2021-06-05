4b8312cc47d995446cbf74f445a986ade61e9d15
Sanskriti Khare <60572554+sanskritikhare142@users.noreply.github.com>
2021-06-06 04:24:16 +0530

    🔧  Update login, signup schema

--- src/utils/schemas.ts
+++ src/utils/schemas.ts
@@ -1,9 +1,31 @@
-import * as yup from "yup"
+import * as yup from "yup";
 
-export const validationSchema = yup.object({
-    title: yup.string().trim().required("This is a required field"),
-    url: yup.string()
-        .trim()
-        .url()
-        .required("This is a required field"),
+export const loginSchema = yup.object({
+  email: yup
+    .string()
+    .trim()
+    .email("Email must be a valid email")
+    .required("This is a required field"),
+  password: yup
+    .string()
+    .trim()
+    .min(6, "Password must have at least 6 characters")
+    .required("This is a required field"),
+});
+
+export const signupSchema = yup.object({
+  email: yup
+    .string()
+    .trim()
+    .email("Email must be a valid email")
+    .required("This is a required field"),
+  password: yup
+    .string()
+    .trim()
+    .min(6, "Password must have at least 6 characters")
+    .required("This is a required field"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Passwords must match")
+    .required("This is a required field"),
 });
